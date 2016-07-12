13d9bf612ac18f409c2862039010cf4f95cd5aa9
jaroslavtyc <mail@jaroslavtyc.com>
2016-07-12 18:13:21 +0200

    Test of roll against trap using a base property can uses shareable core

--- DrdPlus/Tests/RollsOn/Traps/RollOnWillTest.php
+++ DrdPlus/Tests/RollsOn/Traps/RollOnWillTest.php
@@ -1,25 +1,6 @@
 <?php
-namespace DrdPlus\RollsOn\Traps;
+namespace DrdPlus\Tests\RollsOn\Traps;
 
-use Drd\DiceRoll\Templates\Rollers\Roller2d6DrdPlus;
-use DrdPlus\Properties\Base\Will;
-
-class RollOnWillTest extends \PHPUnit_Framework_TestCase
+class RollOnWillTest extends AbstractRollOnPropertyTest
 {
-    /**
-     * @test
-     */
-    public function I_can_use_it()
-    {
-        $rollOnWill = new RollOnWill(
-            $will = Will::getIt($willValue = 123),
-            $roll = Roller2d6DrdPlus::getIt()->roll()
-        );
-        self::assertSame($will, $rollOnWill->getWill());
-        self::assertSame($willValue, $rollOnWill->getPreconditionsSum());
-        self::assertSame($roll, $rollOnWill->getRoll());
-        $resultValue = $willValue + $roll->getValue();
-        self::assertSame($resultValue, $rollOnWill->getValue());
-        self::assertSame((string)$resultValue, (string)$rollOnWill);
-    }
 }
